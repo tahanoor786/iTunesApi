053bdd258dc6712495f8e7f21c324696426743ad
Taha Noor <tahanoor78655@gmail.com>
2023-02-23 20:27:14 +0500

    Initial Commit

---
+++ iTunesApi/iTunesApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTunesApiApp.swift
+//  iTunesApi
+//
+//  Created by Taha Noor on 2/23/23.
+//
+
+import SwiftUI
+
+@main
+struct iTunesApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
